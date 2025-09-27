3e132037d1b1035ae5525f0b884fe862fb27595c
cbekios <constantino.bekios@alumnos.ucn.cl>
2025-09-26 23:44:29 -0300

    x mientras creador de PCs al array :P

--- Taller2POO/src/logica/Main.java
+++ Taller2POO/src/logica/Main.java
@@ -1,15 +1,52 @@
 package logica;
-
+/*
+Constantino Bekios 21761616-6
+Luis Molina 21564225-9
+*/
+import java.io.*;
+import java.util.*;
 import dominio.*;
 
 public class Main {
+	
+	static ArrayList<Computador> PCs = new ArrayList<>();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		System.out.println("Hello chicken");
 		
 		MenuAdmin.abrirMenuAdmin();
+		
+		
 
 	}
+	
+	public static void cargarDatos() throws FileNotFoundException
+	{
+		File compus = new File("pcs.txt");
+		
+		if (!compus.exists()) {
+			throw new FileNotFoundException("ERROR: Archivo no fue encontrado");
+			}
+		
+		
+		Scanner pcscan = new Scanner(compus);
+		
+		String[] pcParts = pcscan.nextLine().split("|");
+		int x=0;
+		
+		while (pcscan.hasNextLine())
+		{
+			x++;
+			int IDadd = x;
+			String IPadd = pcParts[1];
+			String OSadd = pcParts[2];
+			PCs.add(new Computador(IDadd, IPadd, OSadd, null, 0));
+			pcParts = pcscan.nextLine().split("|");
+		}
+		
+		pcscan.close();
+	}
+	
 
 }
