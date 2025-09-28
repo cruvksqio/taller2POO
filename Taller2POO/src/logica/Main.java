015e6e3c783b0bdf06ceee440667af2bbe888776
minom <minom@DESKTOP-HOU7I1D>
2025-09-28 00:45:05 -0300

    user login very nice

--- Taller2POO/src/logica/Main.java
+++ Taller2POO/src/logica/Main.java
@@ -4,25 +4,110 @@ Constantino Bekios 21761616-6
 Luis Molina 21564225-9
 */
 import java.io.*;
+import java.security.MessageDigest;
 import java.util.*;
 import dominio.*;
 
 public class Main {
 	
+	
 	static ArrayList<Computador> PCs = new ArrayList<>();
 	static ArrayList<Puerto> Puertos = new ArrayList<>();
-
+	
 	public static void main(String[] args) throws FileNotFoundException {
-
-		System.out.println("Hello chicken");
 		
 		cargarDatos();
 		
-		MenuAdmin.abrirMenuAdmin();
+		Scanner loginscan = new Scanner(System.in);
 		
+		 String username;
+		 String passcode;
+		 
+		 ArrayList<User> usuarios = cargarUsuarios("usuarios.txt");
+		 
+		System.out.println("--- Bienvenido ---");
+		System.out.println("");
+		System.out.println("Ingrese nombre de usuario :");
+		
+		username = loginscan.nextLine();
+		
+		System.out.println("Ingrese contraseña :");
+		
+		passcode = loginscan.nextLine();
+		
+		String role = validarLogin(usuarios, username, passcode);
+		
+		if (role.equals("ADMIN")) {
+				System.out.println("Login exitoso. Rol: " + role);
+				MenuAdmin.abrirMenuAdmin();
+		}
+		if (role.equals("USER")) {
+				System.out.println("Login exitoso. Rol: " + role);
+		}
+		
+		if (role.equals(null)) {
+			System.out.println("Usuario o contraseña incorrectos");
+		}
+		loginscan.close();
 		
 
 	}
+		
+	public static ArrayList<User> cargarUsuarios(String archivo) throws FileNotFoundException {
+		ArrayList<User> listaUsuarios = new ArrayList<>();
+		File file = new File(archivo);
+		Scanner readit = new Scanner(file);
+		
+		while(readit.hasNextLine()) {
+			String [] p = readit.nextLine().split(";");
+			String user = p[0];
+			String hashedpword = p[1];
+			String role = p[2];
+			
+			User u = new User(user, hashedpword, role);
+			listaUsuarios.add(u);
+			
+		}
+		
+		for (User i : listaUsuarios) {
+			System.out.println(i);
+	}
+		readit.close();
+		return listaUsuarios;
+		
+		
+	}
+	
+	public static String validarLogin(ArrayList<User> usuarios, String username, String passcode) {
+		for (User u : usuarios) {
+			if (u.getUsername().equals(username)) {
+				String hashedInput = sha256Base64(passcode);
+				if (hashedInput != null && hashedInput.equals(u.getPassword())) {
+					return u.getRole();
+				}
+				
+			}
+		}
+		return null; // el usuario no es valido 
+	}
+	
+	private static String sha256Base64(String passcode) {
+		// TODO Auto-generated method stub
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			byte[] digest = md.digest(passcode.getBytes());
+			
+			return Base64.getEncoder().encodeToString(digest);
+		} catch (Exception e) {
+			
+			return null;
+		}
+		
+	}
+	
+		
+		
+	
 	
 	public static void cargarDatos() throws FileNotFoundException
 	{
